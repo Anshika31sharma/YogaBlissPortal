9a465e7546f31f142097fd54b168e7a1b5bab337
Anshika31sharma <sharma.anshika7251@gmail.com>
2024-01-31 02:38:00 +0530

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
-import HomePage from './Components/HomePage';
-// import ProductPage from './Components/ProductPage';
-// import YogaBatchPage from './Components/YogaBatch';
-// import LoginPage from './Components/LoginPage';
+import Carousel from './Components/Carousel';
 import Navbar from './Components/Navbar';
+import HomePage from './Components/HomePage';
 
 function App() {
   useEffect(() => {
@@ -18,15 +16,29 @@ function App() {
     localStorage.setItem('isDarkMode', String(isDarkMode));
   };
 
+  const slides = [
+    'https://plus.unsplash.com/premium_photo-1700061653229-bed0c5e19c8d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHlvZ2ElMjBjbGFzcyUyMHdvbWVufGVufDB8fDB8fHww',
+    'https://plus.unsplash.com/premium_photo-1661901730579-2cd8e29ba20b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8eW9nYSUyMGNsYXNzJTIwd29tZW58ZW58MHwwfDB8fHww',
+    'https://images.unsplash.com/photo-1524863479829-916d8e77f114?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHlvZ2ElMjBjbGFzcyUyMHdvbWVufGVufDB8fDB8fHww',
+    'https://images.unsplash.com/photo-1510894347713-fc3ed6fdf539?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW9nYSUyMGNsYXNzJTIwd29tZW58ZW58MHwwfDB8fHww',
+    'https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8eW9nYSUyMGNsYXNzJTIwd29tZW58ZW58MHwwfDB8fHww',
+    'https://images.unsplash.com/photo-1575052814086-f385e2e2ad1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHlvZ2ElMjBjbGFzcyUyMHdvbWVufGVufDB8MHwwfHx8MA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1663047358310-4cb1d4581807?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHlvZ2ElMjBjbGFzcyUyMHdvbWVufGVufDB8MHwwfHx8MA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1664299192011-b09d1fc8b1d8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fHlvZ2ElMjBjbGFzcyUyMHdvbWVufGVufDB8MHwwfHx8MA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1679522165628-fc1056f8bb35?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fHlvZ2ElMjBjbGFzcyUyMHdvbWVufGVufDB8MHwwfHx8MA%3D%3D',
+  
+  ];
+
   return (
     <>
       <Navbar toggleDarkMode={toggleDarkMode} />
-      <HomePage />
-      {/* <ProductPage /> */}
-      {/* <YogaBatchPage /> */}
-      {/* <LoginPage /> */}
+      <Carousel autoSlide={true}>
+          {[...slides.map((s, i) => <img key={i} src={s} className=' w-full ' />)]}
+      </Carousel>
+      <HomePage/>
+       
     </>
   );
-};
+}
 
 export default App;
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
-
+import React, { useRef } from 'react';
+import yogaVideo from '../Assets/yoga.mp4';
+import icon1 from '../Assets/icon1.jpg';
+import icon2 from '../Assets/icon2.jpg';
+import icon3 from '../Assets/icon3.jpg';
 function HomePage() {
+  const videoRef = useRef(null);
+
+  const handlePlay = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  const handlePause = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
+  };
+
   return (
     <>
-    
+      <div className="flex mt-10">
+        <div className="w-1/2 ml-10">
+          <h1 className="text-4xl font-bold mb-4">Yoga for Mind</h1>
+          <p className="text-lg">Watch our Lead Yoga Therapist.</p>
+         
+        </div>
+        <div className="w-1/2">
+          <video className="max-w-xl rounded-lg" ref={videoRef} controls>
+            <source src={yogaVideo} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div className='flex gap-3  items-center justify-center '>
+      <div className=" h-64 w-80 bg-white dark:bg-slate-300  shadow-lg mt-10 flex flex-col items-center justify-center">
+  <img
+    src={icon1}
+    alt="Card Image"
+    className=" h-24"
+  />
+  <h1 className="text-center font-extrabold text-3xl mt-5">Hard to feel calm</h1>
+  <button className=' w-48 rounded hover:bg-red-400 border border-red-400 mt-12'> Book trial</button>
+</div>
+<div className=" h-64 w-80 bg-white dark:bg-slate-300 ml-20 shadow-lg mt-10 flex flex-col items-center justify-center">
+  <img
+    src={icon2}
+    alt="Card Image"
+    className=" h-24"
+  />
+  <h1 className="text-center font-extrabold text-3xl mt-5">If you have breathing problem</h1>
+  <button className=' w-48 rounded hover:bg-red-400 border border-red-400 mt-5'> Book trial</button>
+</div>
+<div className="h-64 w-80 bg-white dark:bg-slate-300 ml-20 shadow-lg mt-10 flex flex-col items-center justify-center">
+  <img
+    src={icon3}
+    alt="Card Image"
+    className="h-24"
+  />
+  <h1 className="text-center font-extrabold text-3xl mt-5">You have avoided physical activity.</h1>
+  <button className='w-48 rounded hover:bg-red-400 border border-red-400 mt-5'>Book trial</button>
+</div>
+
+      </div>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
